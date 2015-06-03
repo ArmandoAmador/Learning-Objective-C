9d4524df6097b84d1d7b81f0734e32b2bd861c14
Armando Amador <arm.amador@gmail.com>
2015-06-03 00:30:10 -0400

    Creating arrays in C

---
+++ hello_arrays/MyFirstCProgram/MyFirstCProgram/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    float numbers_geeks_love[3];
+    numbers_geeks_love[0] = 3.1415;
+    numbers_geeks_love[1] = 1.6180;
+    numbers_geeks_love[2] = 1.4142;
+    
+    printf("PI %f\n", numbers_geeks_love[0]);
+    printf("golden ratio %f\n", numbers_geeks_love[1]);
+    printf("square root of %f\n", numbers_geeks_love[2]);
+    
+    int primes[] = {2, 3, 5, 7};
+    printf("the first 4 prime numbers %d %d %d %d\n", primes[0], primes[1], primes[2], primes[3]);
+    return 0;
+}
+
