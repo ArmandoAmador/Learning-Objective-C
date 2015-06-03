dff6227130b85d2dd77f9cbf0909dc3f2bc223e2
Armando Amador <arm.amador@gmail.com>
2015-06-03 01:01:53 -0400

    playing around with operators and expressions in C

---
+++ operators_and_expressions/MyFirstCProgram/MyFirstCProgram/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 9;
+    printf("a %d\n", a);
+    
+    int c = a++;
+    printf("c %d a %d\n", c, a);
+    
+    int d = a--;
+    printf("d %d a %d\n", d, a);
+    
+    int b = a % 4;
+    printf("b %d a %d\n", b, a);
+    
+    float fa = 5.0/ .5;
+    printf("fa %f\n", fa);
+    
+    fa *= 10;
+    printf("fa %f\n", fa);
+    
+    return 0;
+}
+
