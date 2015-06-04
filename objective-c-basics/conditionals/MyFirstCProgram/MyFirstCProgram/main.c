ada04b950a47c3e2b38448307cafba095e6c7f4a
Armando Amador <arm.amador@gmail.com>
2015-06-03 23:08:47 -0400

    Working with switch conditionals

--- objective-c-basics/conditionals/MyFirstCProgram/MyFirstCProgram/main.c
+++ objective-c-basics/conditionals/MyFirstCProgram/MyFirstCProgram/main.c
@@ -18,16 +18,35 @@ int main()
     printf("%c %d is the minimum\n", min_value, minimum );
     
     
-    
     char c = 'c';
     char d = 'd';
     char g = 'g';
     
-    char letter = 'z';
+    char letter = 'c';
+    
+    // if-else statement conditional
     if (letter == c) printf("letter %c is %c\n", letter, c);
     else if (letter == d) printf("letter %c is %c\n", letter, d);
     else if (letter == g) printf("letter %c is %c\n", letter, g);
     else printf("letter %c not found\n", letter);
+    
+    // switch statement conditional
+    switch (letter) {
+        case 'c':
+            printf("letter %c is %c\n", letter, c);
+            break;
+            
+        case 'd':
+            printf("letter %c is %c\n", letter, d);
+            break;
+        case 'g':
+            printf("letter %c is %c\n", letter, g);
+            break;
+        default:
+            printf("letter %c not found\n", letter);
+            break;
+    }
+    
     return 0;
 }
 
