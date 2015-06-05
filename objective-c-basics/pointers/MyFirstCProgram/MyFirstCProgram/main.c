b8cab6155f0ec23abde230bda9685bf959596903
Armando Amador <arm.amador@gmail.com>
2015-06-05 01:27:46 -0400

    Learn pointers and structs in C

---
+++ objective-c-basics/pointers/MyFirstCProgram/MyFirstCProgram/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    char *letter;
+    
+    char c = 'k';
+    
+    letter = &c;
+    printf("%c is always the same as %c\n", c, *letter);
+    
+    c = 'q';
+    printf("%c is always the same as %c\n", c, *letter);
+    
+    c = 'x';
+    printf("%c is always the same as %c\n", c, *letter);
+    
+    char eldridge[] = "eldridge";
+    
+    letter = &eldridge[3];
+    printf("%c is in %s\n", *letter, eldridge);
+    
+    ++letter;
+    printf("%c is in %s\n", *letter, eldridge);
+    
+    ++letter;
+    printf("%c is in %s\n", *letter, eldridge);
+    
+    return 0;
+}
+
